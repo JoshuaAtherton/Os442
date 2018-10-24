cab98e8391298ad5a5f4e0cbe1853a72fc5e1c46
Joshua <joshua9@uw.edu>
2018-10-23 21:08:52 -0700

    refactored

--- mash.c
+++ mash.c
@@ -3,64 +3,71 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <assert.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 #define MAXSTR 255
 #define ARGCNT 6
 
 void parse(char ***data, char *cmd); 
 
-int main(int argc, char *argv[])
+/*
+ * Driver for program. 
+ */
+int main(int argc, char *argv[]) {
 
-{
+    char cmd1[MAXSTR];
+    char cmd2[MAXSTR];
+    char cmd3[MAXSTR];
+    char file[MAXSTR];
 
-char cmd1[MAXSTR];
-char cmd2[MAXSTR];
-char cmd3[MAXSTR];
-char file[MAXSTR];
+    // This will get command 1;
+    printf("mash-1->");
+    fgets(cmd1, MAXSTR, stdin);
 
-// This will get command 1;
-printf("mash-1->");
-fgets(cmd1, 100, stdin);
+    // This will get command 2;
+    printf("mash-2->");
+    fgets(cmd2, MAXSTR, stdin);
 
-// This will get command 2;
-printf("mash-2->");
-fgets(cmd2, 100, stdin);
+    // This will get command 3;
+    printf("mash-3->");
+    fgets(cmd3, MAXSTR, stdin);
 
-// This will get command 3;
-printf("mash-3->");
-fgets(cmd3, 100, stdin);
+    // This will get file
+    printf("File->");
+    fgets(file, MAXSTR, stdin);
 
-// This will get file
-printf("File->");
-fgets(file, 100, stdin);
+    char ** args1;
+    parse(&args1, cmd1);
 
-char ** args1;
-parse(&args1, cmd1);
+    char ** args2;
+    parse(&args2, cmd2);
 
-char ** args2;
-parse(&args2, cmd2);
+    char ** args3;
+    parse(&args3, cmd3);
 
-char ** args3;
-parse(&args3, cmd3);
+    //THIS IS FOR OUTPUTING ONLY
+    // for (int i=0;i<ARGCNT;i++)
+    //     printf("i=%d args1[i]=%s\n",i,*(args1 + i));
+    // printf("\n");
 
-//THIS IS FOR OUTPUTING ONLY
-// for (int i=0;i<ARGCNT;i++)
-//     printf("i=%d args1[i]=%s\n",i,*(args1 + i));
-// printf("\n");
+    // for (int i=0;i<ARGCNT;i++)
+    //     printf("i=%d args2[i]=%s\n",i,*(args2 + i));
+    // printf("\n");
 
-// for (int i=0;i<ARGCNT;i++)
-//     printf("i=%d args2[i]=%s\n",i,*(args2 + i));
-// printf("\n");
-
-// for (int i=0;i<ARGCNT;i++)
-//     printf("i=%d args3[i]=%s\n",i,*(args3 + i));
-// printf("\n");
+    // for (int i=0;i<ARGCNT;i++)
+    //     printf("i=%d args3[i]=%s\n",i,*(args3 + i));
+    // printf("\n");
 
 
 
-int status = execvp(args1[0], args1);
-printf("CMD1:[SHELL 1] STATUS CODE=%d\n", status);
+    int status = execvp(args1[0], args1);
+    printf("CMD1:[SHELL 1] STATUS CODE=%d\n", status);
 
-return 0;
+    return 0;
 }
 
 void parse(char ***data, char *cmd) {
